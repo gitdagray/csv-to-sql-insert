8708766a780d784b9e96fd3bc14efbcc8c461e3a
Ashish492 <ashishshrestha492@gmail.com>
2023-12-23 01:29:25 +0545

    ts support

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,40 @@
-import { promises as fs } from "fs";
-
+import { promises as fs ,existsSync} from "fs";
+import {createIfNot} from "./utils/fileUtils"
+import * as  path from "node:path"
 async function writeSQL(statement: string, saveFileAs = "") {
   try {
     const destinationFile = process.argv[2] || saveFileAs;
-
     if (!destinationFile) {
       throw new Error("Missing saveFileAs parameter");
     }
-
+    createIfNot(path.resolve(`/sql/${destinationFile}.sql`))
     await fs.writeFile(`sql/${process.argv[2]}.sql`, statement);
   } catch (err) {
     console.log(err);
   }
 }
-
 async function readCSV(csvFileName = "") {
   try {
     const fileAndTableName = process.argv[2] || csvFileName;
-
     if (!fileAndTableName) {
       throw new Error("Missing csvFileName parameter");
     }
-
+    if(existsSync(path.resolve(`./csv/${fileAndTableName}.csv`))){
+      console.log("file not found")
+      return
+    }
     const data = await fs.readFile(`csv/${fileAndTableName}.csv`, {
       encoding: "utf8",
     });
-
     const linesArray = data.split(/\r|\n/).filter((line) => line);
     const columnNames = linesArray?.shift()?.split(",") || [];
-
     let beginSQLInsert = `INSERT INTO ${fileAndTableName} (`;
     columnNames.forEach((name) => (beginSQLInsert += `${name}, `));
     beginSQLInsert = beginSQLInsert.slice(0, -2) + ")\nVALUES\n";
-
     let values = "";
     linesArray.forEach((line) => {
       // Parses each line of CSV into field values array
       const arr = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-
       if (arr.length > columnNames.length) {
         console.log(arr);
         throw new Error("Too Many Values in row");
@@ -45,7 +42,6 @@ async function readCSV(csvFileName = "") {
         console.log(arr);
         throw new Error("Too Few Values in row");
       }
-
       let valueLine = "\t(";
       arr.forEach((value) => {
         // Matches NULL values, Numbers,
@@ -66,16 +62,12 @@ async function readCSV(csvFileName = "") {
       values += valueLine;
     });
     values = values.slice(0, -2) + ";";
-
     const sqlStatement = beginSQLInsert + values;
-
     // Write File
     writeSQL(sqlStatement);
   } catch (err) {
     console.log(err);
   }
 }
-
 readCSV();
-
 console.log("Finished!");
--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -1,5 +1,5 @@
-import fs from "node:fs"
-export function createIfNot(dir) {
+import * as fs from "node:fs"
+export function createIfNot(dir:string) {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true })
   }
