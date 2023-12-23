f49ad301ad197688ee52c28a7d5d9f488be9d48b
AjayShukla007 <shuklaajay0838@gmail.com>
2023-12-23 13:38:12 +0530

    Added error logging system

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
-import fs from "node:fs/promises"
-import { existsSync } from "node:fs"
-import { createIfNot } from "./utils/fileUtils.mjs"
-import path from "node:path"
+import fs from "node:fs/promises";
+import { existsSync } from "node:fs";
+import { createIfNot } from "./utils/fileUtils.mjs";
+import { logEvents } from "./middleware/logEvents.js";
+import path from "node:path";
 async function writeSQL(statement, saveFileAs = "") {
   try {
-    const destinationFile = process.argv[2] || saveFileAs
+    const destinationFile = process.argv[2] || saveFileAs;
     if (!destinationFile) {
-      throw new Error("Missing saveFileAs parameter")
+      logEvents(`Missing saveFileAs parameter: ${destinationFile}`, "fileSystemError.txt");
+      throw new Error("Missing saveFileAs parameter");
     }
-    createIfNot(path.resolve(`./sql/${destinationFile}`))
-    await fs.writeFile(`sql/${process.argv[2]}.sql`, statement)
+    createIfNot(path.resolve(`./sql/${destinationFile}`));
+    await fs.writeFile(`sql/${process.argv[2]}.sql`, statement);
   } catch (err) {
-    console.log(err)
+    logEvents(`${err}`, "Error.txt");
+    console.log(err);
   }
 }
 async function readCSV(csvFileName = "") {
   try {
-    const fileAndTableName = process.argv[2] || csvFileName
+    const fileAndTableName = process.argv[2] || csvFileName;
     if (!fileAndTableName) {
-      throw new Error("Missing csvFileName parameter")
+      logEvents(`Missing csvFileName parameter: ${fileAndTableName}`, "fileSystemError.txt");
+      throw new Error("Missing csvFileName parameter");
     }
     if (!existsSync(path.resolve(`./csv/${fileAndTableName}.csv`))) {
-      console.log("file not found")
-      return
+      logEvents(`File not found: ./csv${fileAndTableName}`, "fileSystemError.txt");
+      console.log("file not found");
+      return;
     }
     const data = await fs.readFile(`csv/${fileAndTableName}.csv`, {
       encoding: "utf8",
-    })
-    const linesArray = data.split(/\r|\n/).filter(line => line)
-    const columnNames = linesArray.shift().split(",")
-    let beginSQLInsert = `INSERT INTO ${fileAndTableName} (`
-    columnNames.forEach(name => (beginSQLInsert += `${name}, `))
-    beginSQLInsert = beginSQLInsert.slice(0, -2) + ")\nVALUES\n"
-    let values = ""
+    });
+    const linesArray = data.split(/\r|\n/).filter(line => line);
+    const columnNames = linesArray.shift().split(",");
+    let beginSQLInsert = `INSERT INTO ${fileAndTableName} (`;
+    columnNames.forEach(name => (beginSQLInsert += `${name}, `));
+    beginSQLInsert = beginSQLInsert.slice(0, -2) + ")\nVALUES\n";
+    let values = "";
     linesArray.forEach(line => {
       // Parses each line of CSV into field values array
-      const arr = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
+      const arr = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
       if (arr.length > columnNames.length) {
-        console.log(arr)
+        console.log(arr);
+        logEvents(`Too Many Values in row ${arr.length} should be equal to ${columnNames.length}`, "fileSystemError.txt");
         throw new Error("Too Many Values in row")
       } else if (arr.length < columnNames.length) {
+        logEvents(`Too Few Values in row ${arr.length} should be equal to ${columnNames.length}`, "fileSystemError.txt");
         console.log(arr)
         throw new Error("Too Few Values in row")
       }
@@ -55,20 +62,23 @@ async function readCSV(csvFileName = "") {
           else {
             // This wraps strings in quotes
             // also wraps timestamps
-            valueLine += `"${value}", `
+            valueLine += `"${value}", `;
           }
         }
-      })
-      valueLine = valueLine.slice(0, -2) + "),\n"
-      values += valueLine
-    })
-    values = values.slice(0, -2) + ";"
-    const sqlStatement = beginSQLInsert + values
+      });
+      valueLine = valueLine.slice(0, -2) + "),\n";
+      values += valueLine;
+    });
+    values = values.slice(0, -2) + ";";
+    const sqlStatement = beginSQLInsert + values;
     // Write File
-    writeSQL(sqlStatement)
+    writeSQL(sqlStatement);
+    logEvents("Process Finished Successfully", "Success.txt");
   } catch (err) {
-    console.log(err)
+    logEvents(`${err}`, "Error.txt");
+    console.log(err);
   }
 }
-readCSV()
-console.log("Finished!")
+readCSV();
+
+console.log("Finished!");
