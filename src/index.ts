af313a331b16b94839c71c7546a56145907ac335
devlopersabbir <devlopersabbir@gmail.com>
2023-12-25 19:19:20 +0600

    [chore ⚒] converter status added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { promises as fs, existsSync, stat } from "fs";
+import { promises as fs, existsSync } from "fs";
 import * as path from "path";
-import { readLine } from "./utils";
+import { EStatus, readLine } from "./utils";
 import { validateString } from "./validator";
 import { useDirectory } from "./hooks/useDirectory";
+import { response } from "./libs";
 
 class Main {
     private fileName: string = "";
@@ -58,15 +59,16 @@ class Main {
             process.exit(0);
         }
     };
-    writeSQL = async (statement: string, isAppend?: boolean) => {
-        const { write, append, createDirIfNot } = useDirectory();
-        // create directory best on condition
-        createDirIfNot(`${process.cwd()}/sql`);
-        // if append is tru then we don't need to write file
-        if (isAppend) {
-            append(statement, this.destinationFile);
-        } else {
-            write(statement, this.destinationFile);
+    /**
+     * Write SQL code
+     */
+    writeSQL = async (statement: string) => {
+        const { append } = useDirectory();
+        try {
+            await append(statement, this.destinationFile);
+            this.endMessage(EStatus.SUCCESS);
+        } catch (err) {
+            this.endMessage(EStatus.ERROR);
         }
     };
 
@@ -104,7 +106,7 @@ class Main {
             if (index > 0 && index % 500 == 0) {
                 values = values.slice(0, -2) + ";\n\n";
                 // Write File
-                this.writeSQL(`${beginSQLInsert}${values}`, false);
+                this.writeSQL(`${beginSQLInsert}${values}`);
             }
 
             let valueLine = "\t(";
@@ -128,116 +130,35 @@ class Main {
             values += valueLine;
         });
         values = values.slice(0, -2) + ";";
-        this.writeSQL(`${beginSQLInsert}${values}`, true);
-        this.endMessage();
+        this.writeSQL(`${beginSQLInsert}${values}`);
     };
     /**
      * End message
      */
-    endMessage = () => {
-        console.log("==============================");
-        console.log("CSV to SQL convert successfully");
-        process.exit(0);
+    endMessage = (status: EStatus) => {
+        /**
+         * TODO
+         * Handle all status like error pending etc...
+         */
+        switch (status) {
+            case EStatus.SUCCESS:
+                response(this.fileName, this.destinationFile);
+                break;
+            case EStatus.ERROR:
+                response(
+                    this.fileName,
+                    this.destinationFile,
+                    "Fail to convert file!"
+                );
+                break;
+            default:
+                response(
+                    this.fileName,
+                    this.destinationFile,
+                    "Something went wrong!"
+                );
+        }
     };
 }
 
 new Main();
-
-// import { promises as fs ,existsSync} from "fs";
-// import {createIfNot} from "./utils/fileUtils.js"
-// import * as  path from "node:path"
-
-// async function writeSQL(statement: string, saveFileAs = "", isAppend: boolean = false) {
-//   try {
-//     const destinationFile = process.argv[2] || saveFileAs;
-//     if (!destinationFile) {
-//       throw new Error("Missing saveFileAs parameter");
-//     }
-//     createIfNot(path.resolve(`./sql/${destinationFile}.sql`))
-// 		if(isAppend){
-//       await fs.appendFile(`sql/${process.argv[2]}.sql`, statement);
-//     }else{
-//       await fs.writeFile(`sql/${process.argv[2]}.sql`, statement);
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// async function readCSV(csvFileName = "", batchSize: number = 0) {
-//   console.log("read CSV: ", csvFileName, "batch", batchSize)
-//   try {
-//     const fileAndTableName = process.argv[2] || csvFileName;
-
-//     batchSize = parseInt(process.argv[3]) || batchSize || 500;
-// 		let isAppend: boolean = false;
-
-//     if (!fileAndTableName) {
-//       throw new Error("Missing csvFileName parameter");
-//     }
-//     if(!existsSync(path.resolve(`./csv/${fileAndTableName}.csv`))){
-//       console.log("file not found")
-//       return
-//     }
-//     const data = await fs.readFile(`csv/${fileAndTableName}.csv`, {
-//       encoding: "utf8",
-//     });
-//     console.log("sabbir: ",data)
-//     const linesArray = data.split(/\r|\n/).filter((line) => line);
-//     const columnNames = linesArray?.shift()?.split(",") || [];
-//     let beginSQLInsert = `INSERT INTO ${fileAndTableName} (`;
-//     columnNames.forEach((name) => (beginSQLInsert += `${name}, `));
-//     beginSQLInsert = beginSQLInsert.slice(0, -2) + ")\nVALUES\n";
-//     let values = "";
-//     linesArray.forEach((line, index) => {
-//       // Parses each line of CSV into field values array
-//       const arr = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-//       if (arr.length > columnNames.length) {
-//         console.log(arr);
-//         throw new Error("Too Many Values in row");
-//       } else if (arr.length < columnNames.length) {
-//         console.log(arr);
-//         throw new Error("Too Few Values in row");
-//       }
-
-//       // Check batch size (rows per batch)
-//       if(index > 0 && index % batchSize == 0){
-//         values = values.slice(0, -2) + ";\n\n";
-
-//         const sqlStatement = beginSQLInsert + values;
-
-//         // Write File
-//         writeSQL(sqlStatement, fileAndTableName, isAppend);
-//         values = "";
-//         isAppend = true;
-//       }
-
-//       let valueLine = "\t(";
-//       arr.forEach((value) => {
-//         // Matches NULL values, Numbers,
-//         // Strings accepted as numbers, and Booleans (0 or 1)
-//         if (value === "NULL" || !isNaN(+value)) {
-//           valueLine += `${value}, `;
-//         } else {
-//           // If a string is wrapped in quotes, it doesn't need more
-//           if (value.at(0) === '"') valueLine += `${value}, `;
-//           else {
-//             // This wraps strings in quotes
-//             // also wraps timestamps
-//             valueLine += `"${value}", `;
-//           }
-//         }
-//       });
-//       valueLine = valueLine.slice(0, -2) + "),\n";
-//       values += valueLine;
-//     });
-//     values = values.slice(0, -2) + ";";
-//     const sqlStatement = beginSQLInsert + values;
-//     // Write File
-//     writeSQL(sqlStatement, fileAndTableName, isAppend);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-// readCSV();
-// console.log("Finished!");
---
+++ src/utils/enums.ts
@@ -0,0 +1,5 @@
+export enum EStatus {
+    "SUCCESS",
+    "ERROR",
+    "PENDING",
+}
