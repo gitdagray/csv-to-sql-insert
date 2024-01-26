9660bdd6a62cc07b7ce21bd5af1629e3485876b1
Dagogo Clinton Uranta <dagogouranta@gmail.com>
2024-01-26 14:47:10 +0100

    add testing of writeSQL function with Jest testing library

---
+++ src/tests/index.test.ts
@@ -0,0 +1,62 @@
+import { writeSQL } from "../index";
+
+import { promises as fs, existsSync} from "fs";
+
+
+describe('csv to sql test suite', () => {
+    jest.mock('fs', () => ({
+        existsSync: jest.fn(),
+        mkdirSync: jest.fn(),
+        mkdir: jest.fn(),
+        appendFile: jest.fn(),
+        writeFile: jest.fn()
+    }));
+    //const existsSyncSpy = jest.spyOn(fs, 'existsSync');
+    const mkdirSyncSpy = jest.spyOn(fs, 'mkdir');
+    const appendFileSpy = jest.spyOn(fs, 'appendFile');
+    const writeFileSpy = jest.spyOn(fs, 'writeFile');
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should write to a new SQL file exclusively',()=>{
+        //arrange
+       const sqlStatement = `INSERT INTO ExampleTable (id, description, entryDate, location, rate, isActive)
+       VALUES`
+       const filePath = "./ExampleTable.csv"
+       const isAppend = false
+       
+       //act
+         writeSQL(sqlStatement, filePath,isAppend)
+        
+       
+       //assert
+       expect(appendFileSpy).not.toHaveBeenCalled()
+       expect(writeFileSpy).toHaveBeenCalled()
+    
+       
+       })
+    
+    
+    
+    it('should append to an existing SQL file,but not create a new one' ,()=>{
+     //arrange
+    const sqlStatement = `INSERT INTO ExampleTable (id, description, entryDate, location, rate, isActive)
+    VALUES`
+    const filePath = "./ExampleTable.csv"
+    const isAppend = true
+    
+    //act
+     writeSQL(sqlStatement, filePath,isAppend)
+     
+    
+    //assert
+    
+    expect(writeFileSpy).not.toHaveBeenCalled()
+       expect(appendFileSpy).toHaveBeenCalled()
+    
+    
+    })
+
+
+});
